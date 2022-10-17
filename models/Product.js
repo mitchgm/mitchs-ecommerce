91693c84615b7e0d369e7e84c9ed47be25b6c901
mitchgm <mitchgm@bgsu.edu>
2022-10-16 20:01:09 -0700

    set up columns in product model

--- models/Product.js
+++ models/Product.js
@@ -4,12 +4,44 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // this makes an ID column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // creating the product name column
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+    },
+    price: {
+      // this creates the price column and sets its data type
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    // this creates the stock column
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+    },
+    // this creates a category id column
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
+
   },
   {
     sequelize,
