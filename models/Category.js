078b21a44dc3c54afddc87b7238a36e4f72b1c92
mitchgm <mitchgm@bgsu.edu>
2022-10-16 19:43:25 -0700

    added data columns in categories

--- models/Category.js
+++ models/Category.js
@@ -2,11 +2,23 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
-    // define columns
+    id: {
+      // this will add and define an ID column
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+      // this adds the data columns
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // this adds a name for each category column
+    },
   },
   {
     sequelize,
