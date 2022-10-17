00c7559eefe82b6332a08baef41207f9080146d0
mitchgm <mitchgm@bgsu.edu>
2022-10-16 20:09:17 -0700

    added foreign keys to cattegories in models index file

--- models/index.js
+++ models/index.js
@@ -5,12 +5,37 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  // sets the category id as the foreign key that goes through Category
+  foreignKey: 'category_id'
+});
+
+
 
 // Categories have many Products
+Category.hasMany(Product, {
+  // sets the category id as the foreign key that goes through Product
+  foreignKey: "category_id",
+});
+
+
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  // sets the product id as the foreign key that goes through Product
+  foreginKey: "product_id",
+});
+
+
 
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  // sets the tag id as the foreign key that goes through Product
+  foreignKey: "tag_id"
+});
 
 module.exports = {
   Product,
